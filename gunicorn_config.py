499c0e6b033644511e2e6b82a8402cdaca7f18d1
breezee-eastern <154994895+breezee-eastern@users.noreply.github.com>
2023-12-27 09:53:17 -0500

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,2 @@
-python-telegram-bot
-flask
-requests
-gunicorn
-Flask-SSLify
+bind = "0.0.0.0:8000"  # Adjust the port as needed
+workers = 8  # You can adjust the number of workers based on your server's resources
